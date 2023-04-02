d3363966a179ec9299b3c8d57319ab1f06ab730e
Chandan <ch7.pandey@gmail.com>
2023-04-02 17:32:57 +0530

    --Added icons for each link --Added hover effect to link's icons --Set position to fixed to appear on the bottom of page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+
+
+export default function Footer() {
+  return (
+    <div className='fixed w-full top-[95%] md:top-[92%]'>
+      <nav className=' h-full text-white text-4xl flex justify-center gap-32 items-center'>
+        <a href="https://github.com/chandwho">
+        <FontAwesomeIcon icon={faGithub} className='hover:text-[#022c43]'/>
+        </a>
+        <a href="https://www.linkedin.com/in/chandan-pandey06">
+        <FontAwesomeIcon icon={faLinkedin} className='hover:text-[#022c43]'/>
+        </a>   
+        <a href="">
+        <FontAwesomeIcon icon={faEnvelope} className='hover:text-[#022c43]'/>
+        </a>
+        </nav>
+    </div>
+  )
+}
