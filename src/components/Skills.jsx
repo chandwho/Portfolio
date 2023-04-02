a5888f1fb3a0ca8ae90d8c7e7c4a1ad8e1cc4a13
Chandan <ch7.pandey@gmail.com>
2023-04-02 17:24:16 +0530

    -Added divs for every skill with grid layout -Added skill name to show when div is hovered -Added mobile responsiveness to div's grid

---
+++ src/components/Skills.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import '../App.css'
+import html from '../assets/images/html.png'
+import css from '../assets/images/css.png'
+import js from '../assets/images/js.png'
+import bootstrap from '../assets/images/bootstrap.png'
+import tailwind from '../assets/images/tailwind.svg'
+import react from '../assets/images/react.svg'
+import git from '../assets/images/git.svg'
+import BounceLoader from "react-spinners/BounceLoader";
+import {motion} from 'framer-motion' 
+
+export default function Skills(props) {
+  return (
+    <motion.div className='flex flex-col p-20 w-full min-h-screen justify-center items-start gap-16 md:pl-80'
+      initial={{width:0}}
+      animate={{width:'100%'}}
+      exit={{x:window.innerWidth, transition:{duration:0.5} }}
+    >
+
+{/* x:window.innerWidth, transition:{duration:0.5} */}
+      {props.loading? 
+        <BounceLoader className='absolute m-auto top-1/2'
+          color={props.color}
+          loading={props.loading}
+          size={props.size}
+        />
+        :
+      <>
+
+        <div className='top skill-container grid grid-cols-2 auto-rows-fr gap-16  md:grid-cols-4'
+        
+        >
+          <div>
+          <img src={html} alt="HTML" />
+          </div>
+          <div>
+          <img src={css} alt="CSS" />
+          </div>
+          <div>
+          <img src={bootstrap} alt="Bootstrap" />
+          </div>
+          <div>
+          <img src={tailwind} alt="Tailwind" />
+          </div>
+          <div>
+          <img src={js} alt="JavaScript" />
+          </div>
+          <div>
+          <img src={react} alt="ReactJS" />
+          </div>
+          <div>
+          <img src={git} alt="Git" />
+          </div>
+        </div>  
+      </>
+      }
+    </motion.div>
+  )
+}
