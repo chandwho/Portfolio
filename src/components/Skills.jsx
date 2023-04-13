fc8ab9875109be2f2504f0d12eb03f637adce051
Chandan <ch7.pandey@gmail.com>
2023-04-14 03:27:49 +0530

    -- Added firebase as new skill -- Changed background color of skill containers

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,12 +7,13 @@ import bootstrap from '../assets/images/bootstrap.png'
 import tailwind from '../assets/images/tailwind.svg'
 import react from '../assets/images/react.svg'
 import git from '../assets/images/git.svg'
+import firebase from '../assets/images/firebase.png'
 // import BounceLoader from "react-spinners/BounceLoader";
 import {motion} from 'framer-motion' 
 
 export default function Skills(props) {
   return (
-    <motion.div className='flex flex-col p-20 w-full min-h-screen justify-center items-start gap-16 md:pl-80'
+    <motion.div className='flex flex-col p-20 w-full min-h-screen justify-center items-center lg:items-start gap-16 md:pl-80'
     initial={{opacity:0}}
     animate={{opacity:1}}
     exit={{opacity:0}}
@@ -27,28 +28,31 @@ export default function Skills(props) {
         />
         : */}
       <>
-        <h1 className='text-4xl md:text-6xl font-semibold'>Skills</h1>
+        <h1 className='text-4xl md:text-5xl lg:text-6xl font-semibold text-yellow-400'>Skills</h1>
         <div className='top skill-container grid grid-cols-2 auto-rows-fr gap-16  md:grid-cols-3 lg:grid-cols-4'>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={html} alt="HTML" />
           </div>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={css} alt="CSS" />
           </div>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={bootstrap} alt="Bootstrap" />
           </div>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={tailwind} alt="Tailwind" />
           </div>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={js} alt="JavaScript" />
           </div>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={react} alt="ReactJS" />
           </div>
-          <div>
+          <div className='shadow-lg shadow-black'>
           <img src={git} alt="Git" />
+          </div>
+          <div className='shadow-lg shadow-black'>
+          <img src={firebase} alt="Firebase" className='w-[70px]'/>
           </div>
         </div>  
       </>
